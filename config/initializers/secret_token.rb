9064b28e8fe48e47aed096c06c6334b332cb4cad
Michael McIntyre <michael@tekanics.com>
2013-06-11 15:07:39 -0400

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MobileInbox::Application.config.secret_token = '9e8d0ab9f95e3e30d5bade72475c2b54d5fab11bb83290b5297322efa0b7f9a2d15fb74f9f794a42f78bb18bd9f625c57e3a539dc1621491af72b9b6b25579bf'
